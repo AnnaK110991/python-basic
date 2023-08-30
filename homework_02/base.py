11e91821fce90cb2b71cc7a25293925d8128aceb
a.kovalevskaya <a.kovalevskaya@qiwi.com>
2023-08-30 15:21:17 +0300

    homework2_done

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,32 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    started = False
+    fuel = 40
+    fuel_consumption = 6
+    weight = 200
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.started  == False:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise LowFuelError
+        return self.started
+
+
+    def move (self, distance):
+        if distance * self.fuel_consumption <= self.fuel:
+            self.fuel = self.fuel - distance * self.fuel_consumption
+        else:
+            raise NotEnoughFuel
+
+
+
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,30 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+#from homework_02.engine import Engine
+
+class Plane(Vehicle):
+    cargo = 0
+    max_cargo  = None
+
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+         super().__init__(weight=weight, fuel=fuel, fuel_consumption=fuel_consumption)
+         self.max_cargo  = max_cargo
+
+    def load_cargo (self, num):
+        if num + self.cargo <= self.max_cargo:
+            self.cargo = num + self.cargo
+        else:
+            raise CargoOverload
+
+
+    def remove_all_cargo (self):
+        cargo_befor_null  = self.cargo
+        self.cargo = 0
+        return cargo_befor_null
+
+
